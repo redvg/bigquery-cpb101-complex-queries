a58c20651cf928bd027a2b7c27d709ee99a1c271
redvg <38582053+redvg@users.noreply.github.com>
2018-08-02 11:57:51 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 /*
-delays from LGA
+delays from La Guardia
 */
 
 SELECT
@@ -13,4 +13,53 @@ WHERE
   AND departure_airport = 'LGA'
 LIMIT
   100
+  
+/*
+returns the total number of flights (each flight has departure delay?) by each airline from La Guardia,
+*/
+  
+SELECT
+  airline,
+  COUNT(departure_delay) as foo
+FROM
+   `bigquery-samples.airline_ontime_data.flights`
+WHERE
+  departure_airport = 'LGA'
+  AND date = '2008-05-13'
+GROUP BY
+  airline
+ORDER BY airline
+
+/*
+returns the total number of DELAYED flights by each airline from La Guardia
+*/
+
+SELECT
+  airline,
+  COUNT(departure_delay) as delayed
+FROM
+   `bigquery-samples.airline_ontime_data.flights`
+WHERE
+  departure_delay > 0 AND
+  departure_airport = 'LGA'
+  AND date = '2008-05-13'
+GROUP BY
+  airline
+ORDER BY airline
+
+/*
+return both the number delayed as well as the total number of flights
+*/
+
+SELECT
+  airline,
+  COUNT(departure_delay) AS total,
+  SUM(IF(departure_delay > 0, 1, 0)) AS delayed
+FROM
+   `bigquery-samples.airline_ontime_data.flights`
+WHERE
+  departure_airport = 'LGA' AND date = '2008-05-13'
+GROUP BY
+  airline
+
 
