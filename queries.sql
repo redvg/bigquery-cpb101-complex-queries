2aa41200d91d7eba1259a1a524e713a41a613529
redvg <38582053+redvg@users.noreply.github.com>
2018-08-02 11:34:39 +0200

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,16 @@
+/*
+delays from LGA
+*/
+
+SELECT
+  airline,
+  date,
+  departure_delay
+FROM
+  `bigquery-samples.airline_ontime_data.flights`
+WHERE
+  departure_delay > 0
+  AND departure_airport = 'LGA'
+LIMIT
+  100
+
